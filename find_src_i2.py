9408ea04f6aadaf5305250c67818f0e0f65cc87e
Andrew Gallo <agallo@gwu.edu>
2019-09-26 15:39:00 -0400

    basics working

--- find_src_i2.py
+++ find_src_i2.py
@@ -4,23 +4,31 @@
 Original by William Zhang
 https://github.com/willzhang05/senior-research
 
-modified by akg
-# TODO table format (maybe use Pandas?)
-# TODO sort by PPS
-# TODO add column for AMT URI
-# TODO create m3u
+Script to query I2 router proxy for active multicast routes
 
+modified by akg
+# TODO deduplication needs work
 """
-import sys
+
+
+from argparse import ArgumentParser, RawTextHelpFormatter
+import datetime
+import pandas as pd
+import os
 import requests
 import re
 import time
-import datetime
+from prettytable import PrettyTable
 
-BASE_URL = 'https://routerproxy.grnoc.iu.edu/internet2/'
-FILENAME = './ips.txt'
-FORMAT = './logs/mcast-i2-{:%Y-%m-%d-%H:%M:%S}.log'
-VERBOSE = True
+
+parser = ArgumentParser(description="Query I2 router proxy for active multicast routes", formatter_class=RawTextHelpFormatter)
+
+parser.add_argument("-c", "--cutoff", action='store', default=9, metavar="",
+                    help="minimum number of pps to be included in report\n(default is 9)")
+
+args = parser.parse_args()
+
+cutoff = args.cutoff
 
 
 class Source:
@@ -49,21 +57,19 @@ class Source:
         return hash(self.__repr__())
 
 
-def main():
-    global FILENAME
-    global BASE_URL
-    global VERBOSE
-    global FORMAT
-
+def getData():
+    BASE_URL = 'https://routerproxy.grnoc.iu.edu/internet2/'
+    routers = 'ips.txt'
     devices = set()
-    with open(FILENAME, 'r') as f:
+    with open(routers, 'r') as f:
         ip = f.readline()
         while ip != '':
             devices.add(ip.strip())
             ip = f.readline()
     devices = list(devices)      
     output = set()
-    d = datetime.datetime.now()
+    tdict = {}              # temp dict to be appended to tlist that will be used to create data frame
+    tlist = []              # temp list to be used to create data frame
     for device in devices:
         r = requests.get(BASE_URL + '?method=submit&device=' + device + '&command=show multicast&menu=0&arguments=route detail')
         new_text = re.sub(r'&[^\s]{2,4};|[\r]', '', r.text)
@@ -74,24 +80,65 @@ def main():
             if s_line[0] == '':
                 if 'Group' in fields:
                     s = Source(fields['Source'], fields['Group'], fields['Statistics'], device)
-                    if s.pps > 5:
-                        if VERBOSE:
-                            print(s)
-                            print()
-                        output.add(s)
+                    tdict['group'] = s.group
+                    tdict['packets'] = s.packets
+                    tdict['pps'] = s.pps
+                    tdict['router'] = s.router
+                    tdict['source'] = s.source
+                    tdict['speed'] = s.speed
+                    tlist.append(tdict.copy())
                 fields = dict()
             else:
                 fields[s_line[0]] = ''.join(s_line[1:])
-        time.sleep(2)
-    out_list = sorted(list(output))
-    if VERBOSE:
-        print('-' * 10)
-        for o in out_list:
-            print(o)
-    with open(FORMAT.format(d), 'a+') as f:
-        f.write('\n'.join([str(out) for out in out_list]))
-        
+            if len(tdict) > 0:
+                tlist.append(tdict.copy())
+        time.sleep(5)
+    # create the dataframe
+    a = pd.DataFrame(tlist)
+    # sort it by pps, packets, source, router
+    b = a.sort_values(by=['pps', 'packets', 'source', 'router'], ascending=False)
+    # deduplicate
+    finalData = b.drop_duplicates()
+    return finalData
+
+
+def processData(inFrame, cutoff):
+    """
+    process the dataframe-
+    sort it
+    :param inFrame:
+    :return:
+    """
+    directory = './output/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    timenow = time.strftime("%d%b%Y-%H%M") + '.csv'
+    # write full dataframe to CSV
+    dframefilename = directory + 'Dataframe-' + timenow + '.csv'
+    inFrame.to_csv(dframefilename, index=False)
+    # create pretty table and m3u
+    playlistfilename = 'Playlist-' + timenow + '.m3u'
+    outTable = PrettyTable(['group', 'source', 'pps', 'URI'])
+    allURIs = []
+    for index, row in inFrame.iterrows():
+        group = row['group']
+        source = row['source'][:-3]
+        pps = row['pps']
+        if pps > cutoff:
+            amtURI = "amt://" + source + '@' + group
+            outTable.add_row([group, source, pps, amtURI])
+            allURIs.append(amtURI)
+    print(outTable)
+    with open(directory + playlistfilename, 'w') as f:
+        f.write('\n'.join(allURIs))
+    return
+
+
+def main():
+    mcastFrame = getData()
+    processData(mcastFrame, cutoff)
+
 
 if __name__ == '__main__':
-    main()
+  main()
 
